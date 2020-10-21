08b4a94d78d9f2e92e9b332478fa1a71340bb8d0
Den Jo <den@bighitcorp.com>
2020-10-21 11:35:24 +0900

    Added API tests

--- BookShelfTests/BookShelfTests.swift
+++ BookShelfTests/BookShelfTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import BookShelf
 
-class BookShelfTests: XCTestCase {
+final class BookShelfTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,16 +18,42 @@ class BookShelfTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
+    
+    // MARK: - API
+    // MARK: Autocomplete
+    func testRequestAutocomplete() {
+        let expectation = XCTestExpectation(description: "testRequestAutocomplete")
+        
+        NotificationCenter.default.addObserver(forName: SearchNotificationName.autocompletes, object: nil, queue: nil) { notification in
+            XCTAssertNil(notification.object)
+            expectation.fulfill()
+        }
+        
+        let dataManager = SearchDataManager()
+        dataManager.keyword = "Hosts"
+        
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
+    // MARK: Detail
+    func testRequestDetail() {
+        let expectation = XCTestExpectation(description: "testRequestDetail")
+                
+        NotificationCenter.default.addObserver(forName: DetailNotificationName.detail, object: nil, queue: nil) { notification in
+            XCTAssertNil(notification.object)
+            expectation.fulfill()
+        }
+        
+        let dataManager = DetailDataManager()
+        dataManager.isbn = "9780321856715"
+        XCTAssert(dataManager.requestDetail())
+        wait(for: [expectation], timeout: 5.0)
+    }
 }
