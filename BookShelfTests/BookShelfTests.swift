e10ba0503a3b3e42c55663f11439f1e4416a287a
Den Jo <den@bighitcorp.com>
2020-10-21 12:17:51 +0900

    UI Test - Added UI Tests

--- BookShelfTests/BookShelfTests.swift
+++ BookShelfTests/BookShelfTests.swift
@@ -10,16 +10,16 @@ import XCTest
 
 final class BookShelfTests: XCTestCase {
 
+    // Put setup code here. This method is called before the invocation of each test method in the class.
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    // This is an example of a performance test case.
     func testPerformanceExample() throws {
-        // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
